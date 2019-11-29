269b49023a6cedf0a2cf4e88511b30abf9d1aa3a
Stepan Damrin <stepan.damrin@icloud.com>
2019-11-29 16:31:58 +0300

    Убрал избыточность кода

--- src/main/java/ru/damrin/App.java
+++ src/main/java/ru/damrin/App.java
@@ -18,12 +18,11 @@ public class App {
     calc.setA(stringBox.getArgumentA());
     calc.setB(stringBox.getArgumentB());
     calc.setArithmeticAction(stringBox.getArithmeticAction());
-
     calc.calculation(calc.getA(), calc.getB(), calc.getArithmeticAction());
 
-    if (stringBox.isRightRoman() == true && stringBox.isLeftRoman() == true) {
+    if (stringBox.isRightRoman() && stringBox.isLeftRoman()) {
       System.out.println(roman.toRoman(calc.getConclusion()));
-    } else if (stringBox.isLeftRoman() == false && stringBox.isRightRoman() == false) {
+    } else if (!stringBox.isLeftRoman() && !stringBox.isRightRoman()) {
       System.out.println(calc.getConclusion());
     } else {
       System.out.println(err.getIncorrectFormat());
